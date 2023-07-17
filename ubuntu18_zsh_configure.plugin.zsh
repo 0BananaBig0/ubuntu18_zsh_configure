0cad367c5f3d023333eff1d78c53da97542c7cd6
Huaxiao Liang <62785154+0BananaBig0@users.noreply.github.com>
2023-07-17 15:43:40 +0800

    add env for wsl2 vscode

--- ubuntu18_zsh_configure.plugin.zsh
+++ ubuntu18_zsh_configure.plugin.zsh
@@ -103,8 +103,11 @@ if [ `whoami` = "root" ];then
 fi
 
 
+
 # support wsl2 gui applications
 if grep -q WSL2 /proc/version; then
   LOCAL_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
   export DISPLAY=$LOCAL_IP:0
+  # for wsl2 vscode
+  export DONT_PROMPT_WSL_INSTALL=1
 fi
