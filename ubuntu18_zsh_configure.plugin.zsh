a0dda82956bc263ed8b6d43fcd4d2b78db181a25
Huaxiao Liang <62785154+0BananaBig0@users.noreply.github.com>
2024-09-16 08:12:12 +0800

    Update ubuntu18_zsh_configure.plugin.zsh

--- ubuntu18_zsh_configure.plugin.zsh
+++ ubuntu18_zsh_configure.plugin.zsh
@@ -2,11 +2,11 @@
 if [ -x "/opt/ros/melodic/setup.zsh" ]; then
   source /opt/ros/melodic/setup.zsh
 fi
-if [ -x "/home/banana/catkin_ws/devel/setup.zsh" ]; then
-  source /home/banana/catkin_ws/devel/setup.zsh
+if [ -x "$HOME/catkin_ws/devel/setup.zsh" ]; then
+  source $HOME/catkin_ws/devel/setup.zsh
 fi
-if [ -x "/home/banana/study_ws/devel/setup.zsh" ]; then
-  source /home/banana/study_ws/devel/setup.zsh
+if [ -x "$HOME/study_ws/devel/setup.zsh" ]; then
+  source $HOME/study_ws/devel/setup.zsh
 fi
 # Set ROS Network
 #ifconfig查看你的电脑ip地址
@@ -16,7 +16,10 @@ fi
 
 
 # Set MATLAB
-# alias matlab='/home/banana/Polyspace/R2021a/bin/matlab >/dev/null 2>&1 &'
+if [ -f $HOME/Polyspace/R2021a/bin/matlab ]
+then
+  alias matlab='$HOME/Polyspace/R2021a/bin/matlab >/dev/null 2>&1 &'
+fi
 
 
 
@@ -36,7 +39,7 @@ alias vi='vi -p'
 alias vim='vim -p'
 alias gvim='gvim -p'
 alias nvim='nvim -p'
-alias gnvim='/home/banana/Downloads/program/goneovim-linux/goneovim -p'
+alias gnvim='$HOME/Downloads/program/goneovim-linux/goneovim -p'
 
 
 
@@ -54,8 +57,8 @@ export GOPROXY=https://goproxy.io,direct
 # Set environment variable allow bypassing the proxy for specified repos (optional)
 # export GOPRIVATE=git.mycompany.com,github.com/my/private
 if [ `whoami` = "root" ];then
-  if [ -s "/home/banana/.local/.go" ]; then
-    export PATH="$PATH:/home/banana/.local/.go/bin"
+  if [ -s "$HOME/.local/.go" ]; then
+    export PATH="$PATH:$HOME/.local/.go/bin"
   fi
 fi
 
@@ -64,8 +67,8 @@ fi
 # Set Cargo
 export PATH=$PATH:$HOME/.cargo/bin
 if [ `whoami` = "root" ];then
-  if [ -s "/home/banana/.cargo" ]; then
-    export PATH="$PATH:/home/banana/.cargo/bin"
+  if [ -s "$HOME/.cargo" ]; then
+    export PATH="$PATH:$HOME/.cargo/bin"
   fi
 fi
 if command -v zoxide >/dev/null 2>&1;then
@@ -92,13 +95,13 @@ if [ -s "$HOME/.local" ]; then
   export XDG_DATA_DIRS=$XDG_DATA_DIRS:$HOME/.local/share
 fi
 if [ `whoami` = "root" ];then
-  if [ -s "/home/banana/.local" ];then
+  if [ -s "$HOME/.local" ];then
     #add local bin of normal user.
-    export PATH="$PATH:/home/banana/.local/bin"
+    export PATH="$PATH:$HOME/.local/bin"
     #add new dynamic library
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/banana/.local/lib
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib
     #add man information
-    export XDG_DATA_DIRS=$XDG_DATA_DIRS:/home/banana/.local/share
+    export XDG_DATA_DIRS=$XDG_DATA_DIRS:$HOME/.local/share
   fi
 fi
 
@@ -112,3 +115,24 @@ if grep -q WSL2 /proc/version; then
   # for wsl2 vscode
   export DONT_PROMPT_WSL_INSTALL=1
 fi
+
+
+
+PATH="$HOME/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"$HOME/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"; export PERL_MM_OPT;
+
+
+
+# 启动xrdp服务
+if [ -f /etc/init.d/xrdp ] && [ "$(id -u)" != 0 ] && [ -z "$(pgrep xrdp-sesman)" ]
+then
+     echo "1230" | sudo -S /etc/init.d/xrdp start
+     if [ $? -ne 0 ]
+     then
+             echo "failed to start xrdp service!"
+     fi
+fi
+
