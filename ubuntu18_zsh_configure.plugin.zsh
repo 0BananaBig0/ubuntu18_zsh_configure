a8789292a715ac54a6e69bd8c9177aa10b642da2
Huaxiao Liang <1184903633@qq.com>
2024-12-14 18:43:25 +0800

    reconfigure go paths, cargo paths and qt paths

--- ubuntu18_zsh_configure.plugin.zsh
+++ ubuntu18_zsh_configure.plugin.zsh
@@ -1,7 +1,6 @@
 # Set ROS melodic
 if [ -x "/opt/ros/melodic/setup.zsh" ]; then
   source /opt/ros/melodic/setup.zsh
-  # Set ROS Network
   export ROS_HOSTNAME=$(hostname -I | awk '{print $1}')
   export ROS_MASTER_URI=http://${ROS_HOSTNAME}:11311
 fi
@@ -43,28 +42,21 @@ alias nvim='nvim -p'
 
 
 # Set Go language
-export GOPATH="$HOME/.local/.go"
-export PATH="$PATH:${GOPATH//://bin:}/bin"
-export GO111MODULE=on
-# Set the GOPROXY environment variable
-export GOPROXY=https://goproxy.io,direct
-# Set environment variable allow bypassing the proxy for specified repos (optional)
-# export GOPRIVATE=git.mycompany.com,github.com/my/private
-# Using `whoami` = "root" to replace "$(id -u)" == 0 is also ok.
-if [ "$(id -u)" == 0 ];then
-  if [ -s "$HOME/.local/.go" ]; then
-    export PATH="$PATH:$HOME/.local/.go/bin"
-  fi
+if [ -s "$HOME/.local/.go" ]; then
+  export GOPATH="$HOME/.local/.go"
+  export PATH="$PATH:${GOPATH//://bin:}/bin"
+  export GO111MODULE=on
+  # Set the GOPROXY environment variable
+  export GOPROXY=https://goproxy.io,direct
+  # Set environment variable allow bypassing the proxy for specified repos (optional)
+  # export GOPRIVATE=git.mycompany.com,github.com/my/private
 fi
 
 
 
 # Set Cargo
-export PATH=$PATH:$HOME/.cargo/bin
-if [ "$(id -u)" == 0 ];then
-  if [ -s "$HOME/.cargo" ]; then
-    export PATH="$PATH:$HOME/.cargo/bin"
-  fi
+if [ -s "$HOME/.cargo" ]; then
+  export PATH=$PATH:$HOME/.cargo/bin
 fi
 if command -v zoxide >/dev/null 2>&1;then
   eval "$(zoxide init zsh)"
@@ -88,16 +80,6 @@ if [ -s "$HOME/.local" ]; then
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib
   #add man information
   export XDG_DATA_DIRS=$XDG_DATA_DIRS:$HOME/.local/share
-fi
-if [ "$(id -u)" == 0 ];then
-  if [ -s "$HOME/.local" ];then
-    #add local bin of normal user.
-    export PATH="$PATH:$HOME/.local/bin"
-    #add new dynamic library
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib
-    #add man information
-    export XDG_DATA_DIRS=$XDG_DATA_DIRS:$HOME/.local/share
-  fi
 fi
 
 
@@ -123,11 +105,12 @@ fi
 
 # Configure Qt6
 if [ -d ${HOME}/.Qt6 ]; then
-  export PATH=${PATH}:$HOME/.Qt6/Tools/QtCreator/bin:$HOME/.Qt6/6.8.1/gcc_64/bin
-  export LIBRARY_PATH=${LIBRARY_PATH}:$HOME/.Qt6/6.8.1/gcc_64/lib # Load at compile time
-  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$HOME/.Qt6/6.8.1/gcc_64/lib # Load at run time
-  QT_INCLUDE_DIRS=$(echo $HOME/.Qt6/6.8.1/gcc_64/include/*(/) | tr ' ' ':' | sed 's/:$//')
-  export C_INCLUDE_PATH=${C_INCLUDE_PATH}:$HOME/.Qt6/6.8.1/gcc_64/include:$QT_INCLUDE_DIRS # Load at compile time
-  export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:$HOME/.Qt6/6.8.1/gcc_64/include:$QT_INCLUDE_DIRS # Load at compile time
+  export QTDIR=${HOME}/wsl_shared_folder/.Qt6   # Replace with your Qt install path
+  export PATH=${PATH}:$QTDIR/Tools/QtCreator/bin:$QTDIR/6.8.1/gcc_64/bin
+  export LIBRARY_PATH=${LIBRARY_PATH}:$QTDIR/6.8.1/gcc_64/lib # Load at compile time
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$QTDIR/6.8.1/gcc_64/lib # Load at run time
+  QT_INCLUDE_DIRS=$(echo $QTDIR/6.8.1/gcc_64/include/*(/) | tr ' ' ':' | sed 's/:$//')
+  export C_INCLUDE_PATH=${C_INCLUDE_PATH}:$QTDIR/6.8.1/gcc_64/include:$QT_INCLUDE_DIRS # Load at compile time
+  export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:$QTDIR/6.8.1/gcc_64/include:$QT_INCLUDE_DIRS # Load at compile time
   export QT_QPA_PLATFORM=xcb # Not use wayland
 fi
