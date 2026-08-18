5f3a86b5ed2d84b53b9a25fb8a2243707c712951
Huaxiao Liang <hxliang666@qq.com>
2026-08-18 09:45:32 +0800

    update AT_SPI_BUS setting

--- ubuntu18_zsh_configure.plugin.zsh
+++ ubuntu18_zsh_configure.plugin.zsh
@@ -164,7 +164,7 @@ if grep -q WSL2 /proc/version; then
     export GDK_BACKEND=x11
     unset WAYLAND_DISPLAY
     # To resolve dbind-WARNING **: 17:16:19.076: Couldn't connect to accessibility bus: Failed to connect to socket /tmp/dbus-ly00ASJWzY: Connection refused
-    export AT_SPI_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR:-/run/user/$(id -u)}/at-spi/bus"
+    export NO_AT_BRIDGE=1
 fi
 
 
